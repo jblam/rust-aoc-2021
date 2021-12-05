37e0f7e72f3da2a1fb45684fef1e4dc66dcca35b
Jeremy <jeremy.blamey@gmail.com>
2021-12-05 14:17:20 +1100

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,13 +1,33 @@
 pub fn part1() -> u32 {
-    let input = include_str!("day01/input.txt");
-    let mut l = input.lines().map(|s| s.parse::<u32>()).peekable();
+    let mut l = numbers().peekable();
     let mut count = 0;
     while let Some(line) = l.next() {
         if let Some(next) = l.peek() {
-            let (u, v) = (line.unwrap(), next.as_ref().unwrap());
+            let (u, v) = (line, next);
             let increment = if v > &u { 1 } else { 0 };
             count += increment;
         }
     }
     count
+}
+
+pub fn part2() -> u32 {
+    const LENGTH: usize = 3;
+    let mut v = Vec::with_capacity(LENGTH);
+    let mut count = 0;
+    for (index, n) in numbers().enumerate() {
+        if v.len() < LENGTH {
+            v.push(n);
+        } else {
+            let old_sum: u32 = v.iter().sum();
+            v[index % LENGTH] = n;
+            count += if v.iter().sum::<u32>() > old_sum { 1 } else { 0 };
+        }
+    }
+    count
+}
+
+const INPUT: &str = include_str!("day01/input.txt");
+fn numbers() -> impl Iterator<Item = u32> {
+    INPUT.lines().map(|s| s.parse::<u32>().unwrap())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let _ = stdin().read_line(&mut input).expect("Couldn't read stdin");
     let day = input.trim().parse::<u32>().expect("Input was not a number.");
     match day {
-        1 => println!("Day 1: {}", day01::part1()),
+        1 => println!("Day 1: {}, {}", day01::part1(), day01::part2()),
         _ => println!("Nope."),
     };
 }
