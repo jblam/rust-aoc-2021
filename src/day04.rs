8848f8c2f34f75d9fc3642411d173491ed25a924
Jeremy <jeremy.blamey@gmail.com>
2021-12-23 11:15:27 +1100

    Part 1

--- src/day04.rs
+++ src/day04.rs
@@ -1,9 +1,57 @@
 use std::collections::HashSet;
 
 const SIZE: usize = 5;
+pub const INPUT: &str = include_str!("day04/input.txt");
+
+pub fn part1(input: &str) -> usize {
+    let (numbers, mut boards) = parse_input(input).unwrap();
+    play(&numbers, &mut boards).expect("No board won")
+}
+
+fn play(numbers: &[u8], boards: &mut [Board]) -> Option<usize> {
+    let mut h = HashSet::new();
+    for &n in numbers {
+        h.insert(n);
+        for b in boards.iter_mut() {
+            if b.mark(n) {
+                return Some(b.score(h) * (n as usize));
+            }
+        }
+    }
+    None
+}
 
 fn parse_input(input: &str) -> Result<(Vec<u8>, Vec<Board>), String> {
-    todo!()
+    let (first_line, input) =
+        one_line(input).ok_or_else(|| "Did not start with the call numbers".to_string())?;
+    let numbers = first_line
+        .split(',')
+        .map(|t| t.parse::<u8>())
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|e| e.to_string())?;
+    let (_, input) = match one_line(input) {
+        Some((line, rest)) if line.is_empty() => Ok((line, rest)),
+        _ => Err("Call numbers not followed by an empty line".to_string()),
+    }?;
+    let first = Board::consume(input)?.ok_or("No boards")?;
+    let boards = std::iter::successors(Some(Ok(first)), |prev| {
+        // I'm sure there's a more elegant way to do this, but Clippy is not complaining,
+        // so ?
+        if let Ok((_, input)) = prev {
+            let result = Board::consume(input);
+            match result {
+                Err(e) => Some(Err(e)),
+                Ok(None) => None,
+                Ok(Some(t)) => Some(Ok(t)),
+            }
+        } else {
+            None
+        }
+    });
+    let boards = boards
+        .map(|r| r.map(|t| t.0))
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok((numbers, boards))
 }
 
 fn one_line(input: &str) -> Option<(&str, &str)> {
@@ -18,6 +66,8 @@ struct Board {
     col_score: [u8; SIZE],
 }
 impl Board {
+    /// Marks the number, if present on the board.
+    /// Returns `true` if the board has now won.
     fn mark(&mut self, number: u8) -> bool {
         if let Some(index) = self.numbers.iter().position(|i| i == &number) {
             let col = index % SIZE;
@@ -33,6 +83,8 @@ impl Board {
         }
     }
 
+    /// Calculates the (puzzle-defined) board "score" being the sum of all
+    /// unmarked numbers. Does not validate if the board is already won.
     fn score(&self, numbers: HashSet<u8>) -> usize {
         self.numbers
             .iter()
@@ -41,6 +93,8 @@ impl Board {
             .sum()
     }
 
+    /// Produces a board by consuming some part of the string, or `None`
+    /// if the input is exhausted.
     fn consume(input: &str) -> Result<Option<(Self, &str)>, String> {
         if input.is_empty() {
             Ok(None)
@@ -48,12 +102,14 @@ impl Board {
             let mut arr = [0u8; SIZE * SIZE];
             let mut input = input;
             for i in 0..SIZE {
-                let (line, rest) = one_line(input)
-                    .ok_or(format!("line {} of the board could not be parsed", i))?;
+                let (line, rest) = one_line(input).unwrap_or((input, ""));
                 let part = &mut arr[i * SIZE..][..SIZE];
                 let mut j = 0;
                 for token in line.split_ascii_whitespace() {
-                    part[j] = token.parse().unwrap();
+                    // why do I need to add this error type annotation?
+                    // rust-analyzer didn't need it.
+                    let result: u8 = token.parse().map_err(|op: std::num::ParseIntError| op.to_string())?;
+                    part[j] = result;
                     j += 1;
                 }
                 if j != SIZE {
@@ -62,9 +118,13 @@ impl Board {
                 input = rest;
             }
 
-            let (blank, rest) = input
-                .split_once('\n')
-                .ok_or("Board did not terminate with an empty line".to_string())?;
+            let (blank, rest) = if input.is_empty() {
+                (input, input)
+            } else {
+                input
+                    .split_once('\n')
+                    .ok_or_else(|| "Board did not terminate with an empty line".to_string())?
+            };
             if blank.trim().is_empty() {
                 let output = Self {
                     numbers: arr,
@@ -79,19 +139,18 @@ impl Board {
     }
 }
 
-fn smaller(c: char) -> Option<char> {
-    const BASIS: u32 = 0x2080;
-    if let Some(i) = c.to_digit(10) {
-        char::from_u32(BASIS + i)
-    } else {
-        None
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn smaller(c: char) -> Option<char> {
+        const BASIS: u32 = 0x2080;
+        if let Some(i) = c.to_digit(10) {
+            char::from_u32(BASIS + i)
+        } else {
+            None
+        }
+    }
     #[test]
     fn emits_small_digits() {
         assert_eq!(Some('₁'), smaller('1'));
@@ -103,11 +162,8 @@ mod tests {
     fn marks_board() {
         let mut board = Board {
             numbers: [
-                 0,  1,  2,  3,  4,
-                 5,  6,  7,  8,  9,
-                10, 11, 12, 13, 14,
-                15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24,
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                23, 24,
             ],
             col_score: [0; 5],
             row_score: [0; 5],
@@ -154,5 +210,32 @@ rest"#;
     fn parses_error() {
         let result = Board::consume("not a good line");
         assert!(result.is_err())
+    }
+
+    const TEST_INPUT: &str = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"#;
+    #[test]
+    fn parses_test_input() -> Result<(), String> {
+        let (numbers, mut boards) = parse_input(TEST_INPUT)?;
+        let score = play(&numbers, &mut boards).ok_or("No board won".to_string())?;
+        assert_eq!(4512, score);
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::stdin;
 mod day01;
 mod day02;
 mod day03;
+#[allow(unused)]
 mod day04;
 
 fn main() {
@@ -14,7 +15,7 @@ fn main() {
         1 => println!("Day 1: {}, {}", day01::part1(), day01::part2()),
         2 => println!("Day 2: {}, {}", day02::part1(), day02::part2()),
         3 => println!("Day 3: {}, {}", day03::part1(day03::INPUT), day03::part2(day03::INPUT)),
-        4 => println!("Day 4: {}, {}", "Nope", "Nope"),
+        4 => println!("Day 4: {}, {}", day04::part1(day04::INPUT), "Nope"),
         _ => println!("Nope."),
     };
 }
